b640f9423e493996ca30a6722ec01cf5794df980
sh200000 <ts46144bam.sh.k@outlook.jp>
2022-05-06 16:42:50 +0900

    新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name,            presence: true
+  
+  validates :last_name,       presence: true, format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :first_name,      presence: true, format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  
+  validates :last_name_kana,  presence: true, format:{with: /\A[ァ-ヶ]+\z/}
+  validates :first_name_kana, presence: true, format:{with: /\A[ァ-ヶ]+\z/}
+  validates :birthday,        presence: true
+
+  has_many   :items
+  has_many   :orders
+  
 end
