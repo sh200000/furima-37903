28a8f21b20205c195aebf6435892443180a8f237
sh200000 <ts46144bam.sh.k@outlook.jp>
2022-05-09 19:37:10 +0900

    itemモデル単体テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -57,6 +57,22 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping day must be other than 1")
       end
+      it '価格に半角数字以外が含まれている場合は出品できない' do
+        @item.price = "１"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it '価格が300円未満では出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it '価格が9_999_999円を超えると出品できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+
       it 'userが紐付いていないと保存できない' do
         @item.user = nil
         @item.valid?
