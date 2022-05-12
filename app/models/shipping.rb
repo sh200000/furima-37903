dc1b77b3bbfc706416480dcc09476aa9bd8fe2f5
sh200000 <ts46144bam.sh.k@outlook.jp>
2022-05-12 11:59:42 +0900

    ルーティング・コントローラー作成

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,10 +1,10 @@
 class Shipping < ApplicationRecord
-  validates :post_code, presence: true
+  validates :post_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
   validates :prefecture_id, numericality: { other_than: 1 }
   validates :city, presence: true
   validates :address, presence: true
   validates :building
-  validates :telephone, presence: true
+  validates :telephone, presence: true, format: { with: /\A\d{10,11}\z/ }
 
   belongs_to :order
 end
